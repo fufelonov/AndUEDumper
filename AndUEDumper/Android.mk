a103bb3b347d04ce1cda8d199972ab17f3a26be2
fufelonov <fufelonov@gmail.com>
2026-02-13 23:37:45 +0200

    Update Android.mk to remove library.mk inclusion
    Remove library.mk include and add comments for clarity.

--- AndUEDumper/Android.mk
+++ AndUEDumper/Android.mk
@@ -1,7 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-
 TMP_LOCAL_PATH := $(LOCAL_PATH)
+
+# Оставляем только эту строку для сборки программы
 include $(TMP_LOCAL_PATH)/src/executable.mk
-include $(TMP_LOCAL_PATH)/src/library.mk
+
+# Эту строку удаляем или ставим # в начале
+# include $(TMP_LOCAL_PATH)/src/library.mk
